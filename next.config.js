62d87c76dee0956dcabff584505dde8c36a03f64
liliovo11 <cijianfangli@outlook.com>
2026-07-23 16:15:12 +0800

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const withPWA = require("next-pwa")({
+  dest: "public",
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+});
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "5mb",
+    },
+  },
+};
+
+module.exports = withPWA(nextConfig);
